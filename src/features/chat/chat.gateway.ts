763172edf72cd2967836f1f81fe90449ad6b106f
rachelis19 <44132906+rachelis19@users.noreply.github.com>
2021-09-02 10:13:18 +0300

    chat

--- src/features/chat/chat.gateway.ts
+++ src/features/chat/chat.gateway.ts
@@ -1,39 +1,22 @@
 import {
-    ConnectedSocket,
     MessageBody,
     SubscribeMessage,
     WebSocketGateway,
+    WebSocketServer,
   } from '@nestjs/websockets';
-  import { NestGateway } from '@nestjs/websockets/interfaces/nest-gateway.interface'
   import { ChatService } from './chat.service'
-  import { Bind } from '@nestjs/common'
-  import { Chat } from './chat.schema'
-  
-  @WebSocketGateway()
-  export class ChatGateway implements NestGateway {
+  import { Server } from 'socket.io';
+
+  @WebSocketGateway({ cors: true })
+  export class ChatGateway {
     constructor(private chatService: ChatService) {}
+
+    @WebSocketServer() server: Server
   
-    afterInit(server: any) {
-      console.log('Init', server)
-    }
-  
-    handleConnection(socket: any) {
-      console.log('Connect', socket)
-      process.nextTick(async () => {
-        socket.emit('allChats', await this.chatService.getChats())
-      })
-    }
-  
-    handleDisconnect(socket: any) {
-      console.log('Disconnect', socket)
-    }
-  
-    @Bind(MessageBody(), ConnectedSocket())
-    @SubscribeMessage('chat')
-    async handleNewMessage(chat: Chat, sender: any) {
-      console.log('New Chat', chat);
-      await this.chatService.saveChat(chat)
-      sender.emit('newChat', chat);
-      sender.broadcast.emit('newChat', chat)
+    @SubscribeMessage('Message')
+    async handleNewMessage(@MessageBody() message: string) {
+      console.log(message);
+      
+      this.server.emit('Message', message)
     }
   }
